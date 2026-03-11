9d4a9f5b2086e3a434f5b21f8d372805cf7f5f45
u_z1nee <u_z1nee@naver.com>
2026-03-11 11:23:01 +0900

    change 3

--- src/main/java/com/apiece/springboot_twitter/comment/CommentRequest.java
+++ src/main/java/com/apiece/springboot_twitter/comment/CommentRequest.java
@@ -7,6 +7,6 @@ public record CommentRequest(
         @NotBlank(message =
                 "댓글 내용은 비어있을 수 없습니다 !")
         @Size(min = 1, max = 50, message =
-                "댓글은 1자 이상 50자 이하여야 합니다 !")
+                "댓글은 1자 이상 50자 이하여야 합니다 !!")
         String content
 ) {}
